bca6de96fc51d1954cbc375142ca8ab6b92caf59
Darci Neto <netoschneideer@gmail.com>
2019-12-08 17:46:18 -0300

    Aleração nos dados de bd

--- helpers/connection.js
+++ helpers/connection.js
@@ -4,8 +4,8 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '002531',
-    database: 'servicos_informatica'
+    password: 'SUA SENHA',
+    database: 'SUA TABELA'
 });
 
 module.exports = connection;
